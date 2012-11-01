ccdc7a4b91357e42d714f45f399a48f97baa905b
syron <rwlmayer@gmail.com>
2012-11-01 23:20:22 +0100

    Added milestone controller
    Added milestone controller with standard functions to get implemented.

---
+++ app/Controller/MilestonesController.php
@@ -0,0 +1,10 @@
+<?php
+	class MilestonesController extends AppController {
+		public function index() {}
+		public function add() {}
+		public function delete() {}
+		public function restore() {}
+		public function milestones($projectId) {}
+		public function milestone($projectId, $milestoneId) {}
+	}
+?>
