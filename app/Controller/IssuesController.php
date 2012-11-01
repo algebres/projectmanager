30034436036388341701e2ca1fa48aee97ff1551
syron <rwlmayer@gmail.com>
2012-11-01 23:48:09 +0100

    Added issue and task controller
    Issue and task controller with basic functions.

---
+++ app/Controller/IssuesController.php
@@ -0,0 +1,9 @@
+<?php
+	class IssuesController extends AppController {
+		public function index() {}
+		public function add() {}
+		public function delete() {}
+		public function issues($projectId) {}
+		public function issue($projectId, $taskId) {}
+	}
+?>
---
+++ app/Controller/TasksController.php
@@ -0,0 +1,9 @@
+<?php
+	class TasksController extends AppController {
+		public function index() {}
+		public function add() {}
+		public function delete() {}
+		public function tasks($projectId) {}
+		public function task($projectId, $taskId) {}
+	}
+?>
